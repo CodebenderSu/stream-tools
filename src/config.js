4d085215ec52522a013d52fbe543ef09a5691790
CodebenderSu <codebendersu@gmail.com>
2020-06-06 21:15:40 -0400

    Adjusting defaults for countdown

--- src/config.js
+++ src/config.js
@@ -6,13 +6,13 @@ export const routes = {
 };
 
 export const countdownOptions = {
-  s: 0, // Seconds for counting down to (TAKES PRIORITY)
+  s: 360, // Seconds for counting down to (TAKES PRIORITY)
   timestamp: 0, // UNIX date timestamp to count down to
   ms: false, // Boolean for displaying milliseconds
   mins: true, // Boolean for displaying minutes
   hrs: false, // Boolean for displaying hours
   days: false, // Boolean for displaying days
-  prefix: '', // Message to display before countdown timer
+  prefix: 'in', // Message to display before countdown timer
   suffix: '', // Message to display after countdown timer
-  complete: 'Completed!' // Message to display when countdown is finished
+  complete: 'imminently...' // Message to display when countdown is finished
 };
